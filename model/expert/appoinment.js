dcff4761a2c465e24e975e1e3aaafbac6d3e540e
assim fayas <asim1fayas@gmail.com>
2023-10-01 23:40:53 +0530

    modified appoinment model

--- model/expert/appoinment.js
+++ model/expert/appoinment.js
@@ -4,12 +4,6 @@ const Schema = mongoose.Schema;
 const appointmentSchema = new Schema({
     expert: { type: Schema.Types.ObjectId, ref: 'Expert', required: true },
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    scheduledAt: {
-        slot_time: { type: String, required: true },
-        slot_date: { type: String, required: true },
-        date: { type: Date, required: true },
-        isBooked: { type: Boolean, default: true }
-    },
     consultingFee: { type: Number, required: true },
     isConsulted: { type: Boolean, default: false },
     bookingType: { type: String, enum: ['video', 'chat'] },
@@ -22,15 +16,45 @@ const appointmentSchema = new Schema({
         type: String,
         enum: ['consulted', 'cancelled', 'notConsulted', 'cancellation-requested'],
         default: 'notConsulted'
-    }, AppoinmentStatus: {
+    },
+    AppoinmentStatus: {
         type: String,
         enum: ["active", "expired"],
         default: "active"
-
     },
+    scheduledAt: {
+        slot_time: { type: String },
+        slot_date: { type: String },
+        date: { type: Date }
+    }
 }, {
-    timestamps: { createdAt: 'created_at' } // You can specify a custom field name for the created timestamp
+    timestamps: { createdAt: 'created_at' }
 });
+
+// Custom validation to make scheduledAt fields required conditionally
+appointmentSchema.path('scheduledAt.date').validate(function (value) {
+    // Only required if bookingType is not 'chat'
+    if (this.bookingType !== 'chat') {
+        return typeof value !== 'undefined';
+    }
+    return true;
+}, 'Scheduled date is required when bookingType is not chat');
+
+appointmentSchema.path('scheduledAt.slot_date').validate(function (value) {
+    // Only required if bookingType is not 'chat'
+    if (this.bookingType !== 'chat') {
+        return typeof value !== 'undefined';
+    }
+    return true;
+}, 'Scheduled slot_date is required when bookingType is not chat');
+
+appointmentSchema.path('scheduledAt.slot_time').validate(function (value) {
+    // Only required if bookingType is not 'chat'
+    if (this.bookingType !== 'chat') {
+        return typeof value !== 'undefined';
+    }
+    return true;
+}, 'Scheduled slot_time is required when bookingType is not chat');
 
 const Appointment = mongoose.model('Appointment', appointmentSchema);
 
